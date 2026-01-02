dc17d348ca32ca44d19647ac297bb84141580f51
Lithika Murugesan <lithika2404@gmail.com>
2026-01-02 16:30:06 +0530

    Update ExpenseFilters.jsx

--- frontend/src/components/ExpenseFilters.jsx
+++ frontend/src/components/ExpenseFilters.jsx
@@ -1,26 +1,3 @@
-// import React from "react";
-// const ExpenseFilters = ({ expenses, setFiltered }) => {
-//   const handleType = (type) => {
-//     if (!type) return setFiltered(expenses);
-//     setFiltered(expenses.filter((e) => e.type === type));
-//   };
-
-//   return (
-//     <div className="flex gap-4 mb-6">
-//       <select
-//         onChange={(e) => handleType(e.target.value)}
-//         className="bg-[#020617] border border-white/10 rounded-lg px-4 py-2"
-//       >
-//         <option value="">All</option>
-//         <option value="income">Income</option>
-//         <option value="expense">Expense</option>
-//       </select>
-//     </div>
-//   );
-// };
-
-// export default ExpenseFilters;
-
 import React from "react";
 
 const ExpenseFilters = ({ expenses, setFiltered }) => {
